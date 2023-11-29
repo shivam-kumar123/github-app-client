14302231e08b22b6f50f6efe7fcc0912d88ec131
shivam kumar <75497119+shivam-kumar123@users.noreply.github.com>
2023-11-29 13:24:40 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,56 +1,33 @@
-// client/components/Login.tsx
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import axios from 'axios';
 
 type Props = {
-  setIsLoggedIn: (isLoggedIn: boolean) => void;
-  setGitHubUsername: (username: string) => void;
-};
+    setIsLoggedIn: (isLoggedIn: boolean) => void;
+}
 
-const Login = ({ setIsLoggedIn, setGitHubUsername }: Props) => {
-  const handleGithubLogin = async () => {
-    window.location.assign(
-      'https://github.com/login/oauth/authorize?client_id=' + process.env.REACT_APP_CLIENT_ID
-    );
-  };
+const Login = ({setIsLoggedIn}: Props) => {
 
-  useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const codeParam = urlParams.get('code');
-
-    const fetchGitHubUser = async () => {
-      if (codeParam !== null) {
-        const response = await axios.post('https://github-app-server.onrender.com', { code: codeParam });
-        const githubToken = response.data.access_token;
-
-        try {
-          // Fetch the GitHub user details using the access token
-          const userResponse = await axios.get('https://api.github.com/user', {
-            headers: {
-              Authorization: `Bearer ${githubToken}`,
-            },
-          });
-
-          const githubUsername = userResponse.data.login;
-
-          // Set the GitHub username in the state
-          setGitHubUsername(githubUsername);
-          setIsLoggedIn(true);
-        } catch (error) {
-          console.error('Error fetching GitHub user details:', error);
-        }
-      }
+    const handleGithubLogin = async () => {
+        window.location.assign("https://github.com/login/oauth/authorize?client_id=" + process.env.REACT_APP_CLIENT_ID);
     };
 
-    fetchGitHubUser();
-  }, [setIsLoggedIn, setGitHubUsername]);
+    useEffect(() => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const codeParam = urlParams.get('code');
+        console.log(codeParam);
+        if (codeParam !== null) {
+            setIsLoggedIn(true);
+        }
+    }, []);
 
   return (
     <div>
-      <button onClick={handleGithubLogin}>Login With Github</button>
+        <button onClick={handleGithubLogin}>
+            Login With Github
+        </button>
     </div>
-  );
-};
+  )
+}
 
 export default Login;
