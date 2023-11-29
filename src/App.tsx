e8e89344ab16a01aa309a1ce9aee688b98bdb3fe
shivam kumar <20051252@kiit.ac.in>
2023-11-29 07:32:43 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
 import Login from './components/Login';
-import PullRequestList from './components/PullRequestList'
+import socketIOClient from 'socket.io-client';
 
-function App() {
-
+const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [webhookPayload, setWebhookPayload] = useState<any>(null);
+  const socket = socketIOClient('https://github-app-server.onrender.com'); // Replace with your server URL
+
+  useEffect(() => {
+    // Listen for 'webhookEvent' from the server
+    socket.on('webhookEvent', (payload) => {
+      console.log('Received webhook event:', payload);
+      setWebhookPayload(payload);
+    });
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      socket.disconnect();
+    };
+  }, [socket]);
 
   return (
     <div className="App">
       Github App
-      <Login 
-        setIsLoggedIn={setIsLoggedIn}
-      />
-      {
-        isLoggedIn && 
+      <Login setIsLoggedIn={setIsLoggedIn} />
+      {webhookPayload && (
         <div>
-          <h3>Pull Requests</h3>
-          <PullRequestList />
+          <h3>Webhook Payload</h3>
+          <pre>{JSON.stringify(webhookPayload, null, 2)}</pre>
         </div>
-      }
+      )}
     </div>
   );
-}
+};
 
 export default App;
